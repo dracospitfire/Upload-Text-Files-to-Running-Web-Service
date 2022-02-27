9f13158fa76f3519f91dfaea7c42769ec1e15d0e
Austin Flores <austin3flores@gmail.com>
2022-02-26 16:00:57 -0800

    Updated script to create dictionary of different reviews from text file
    I have included the text files that I was given to work with.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-print("Hello Wold")
+#! /usr/bin/env python3
 
+import os
+import requests
 
-#This is the only file I want...
+reviews = {}
+for file in os.listdir("."):
+    if file.endswith(".txt"):
+        review = open(file, "r")
+        text = review.readlines()
+        review = {}
+        review.update({"title":text[0].strip("\n")})
+        review.update({"name":text[1].strip("\n")})
+        review.update({"date":text[2].strip("\n")})
+        review.update({"feedback":text[3].strip("\n")})
+        reviews.update({file:review})
 
-
-'''
-lets see if this works
-'''
-
+url = http://http://34.123.148.210//feedback
+requests.post(url, data = reviews)
