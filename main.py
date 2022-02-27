9afad05183c89919314124df8098130a73b3ac2f
Austin Flores <austin3flores@gmail.com>
2022-02-26 20:59:48 -0800

    project submitted and passed server and path needs to be adjusted for each user

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import os
 import requests
 
-reviews = {}
-for file in os.listdir("."):
-    if file.endswith(".txt"):
-        review = open(file, "r")
-        text = review.readlines()
-        review = {}
-        review.update({"title":text[0].strip("\n")})
-        review.update({"name":text[1].strip("\n")})
-        review.update({"date":text[2].strip("\n")})
-        review.update({"feedback":text[3].strip("\n")})
-        reviews.update({file:review})
+url = 'http://###.###.###.###/feedback/'
+path = "." #/data/feedback
 
-url = http://http://34.123.148.210//feedback
-requests.post(url, data = reviews)
+for file in os.listdir(path):
+  if file.endswith(".txt"):
+    file = open(file, "r") #os.path.join(path
+    text = file.readlines()
+    review = {}
+    review.update({"title":text[0].strip("\n")})
+    review.update({"name":text[1].strip("\n")})
+    review.update({"date":text[2].strip("\n")})
+    review.update({"feedback":text[3].strip("\n")})
+    status = requests.post(url, data = review)
+    print(status.status_code)
